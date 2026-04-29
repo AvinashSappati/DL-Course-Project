94a6da6697035cf02640b38dadcde0510f1fdecf
Mohammad Asad ur Rahman Nisar <mdnisaar.j99034@gmail.com>
2026-04-29 17:35:46 +0530

    Updated script.js for public url

--- Frontend/script.js
+++ Frontend/script.js
@@ -26,7 +26,7 @@ predictBtn.addEventListener('click', async () => {
     });
 
     try {
-        const response = await fetch('http://127.0.0.1:8000/predict', {
+        const response = await fetch('/predict', {
             method: 'POST',
             body: formData,
         });
