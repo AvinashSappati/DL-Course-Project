9cee6decc0b00bd01037bb86f2ea8862d01652b8
Mohammad Asad ur Rahman Nisar <mdnisaar.j99034@gmail.com>
2026-04-29 17:33:56 +0530

    Updated app.py for deployment

--- Backend/app.py
+++ Backend/app.py
@@ -1,31 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 from typing import List
+from fastapi.staticfiles import StaticFiles
 from inference import predict_batch
 
-app = FastAPI(title="CNN-RNN Multi-Label Classifier")
-
-# Allow frontend to communicate with backend
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"], 
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = FastAPI(title="CNN-RNN API")
 
 @app.post("/predict")
 async def predict_multiple(files: List[UploadFile] = File(...)):
     try:
-        # Read all files into a list of bytes
         image_bytes_list = [await file.read() for file in files]
         filenames = [file.filename for file in files]
-        
-        # Run batch prediction
         batch_results = predict_batch(image_bytes_list)
-        
-        # Zip filenames and predictions together
         final_output = [{"filename": f, "predictions": p} for f, p in zip(filenames, batch_results)]
-        
         return {"results": final_output}
     except Exception as e:
         return {"error": str(e)}
+
+# Pro-Move: Mount the static frontend folder to the root URL
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
